abd1046e9ac9d6cea2932b1006329f454a0816e2
subhajitgope <subhajitgope5@gmail.com>
2025-06-27 01:06:49 +0530

    your message

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -16,19 +16,19 @@ const Contact = () => {
           data-aos="zoom-in-up"
           data-aos-duration="1000"
         >
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.instagram.com/itz_subhajit_1/" target="_blank" className="items">
             <FaInstagram className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.facebook.com" target="_blank" className="items">
             <CiFacebook className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.linkedin.com/in/subhajit-gope-4a539b274/" target="_blank" className="items">
             <FaLinkedinIn className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.twitter.com" target="_blank" className="items">
             <FaSquareXTwitter className="icons" />
           </a>
-          <a href="https://github.com/subhajitgope" target="_blank" className="items">
+          <a href="https://github.com/subha9645" target="_blank" className="items">
             <FaGithubSquare className="icons" />
           </a>
           <a
